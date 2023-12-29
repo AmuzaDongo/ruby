4056359e0dfa79a9222be522144ccaf7ac630fa3
AmuzaDongo <dongoamuza@gmail.com>
2023-12-29 10:25:27 +0300

    add convert object methods

---
+++ methods_to_convert_objects.rb
@@ -0,0 +1,39 @@
+text = "5"
+
+puts text
+puts text.class
+
+puts text.to_i
+puts text.to_i.class
+
+puts "12 oranges".to_i
+puts "oranges 12".to_i
+puts "oranges".to_i
+
+puts text.to_f
+puts text.to_f.class
+
+puts
+
+number = 10
+puts number.class
+puts number.to_s
+puts number.to_s.class
+
+puts number.to_f
+puts number.to_f.class
+
+
+puts 
+
+percentage = 0.8
+puts percentage.class
+puts percentage.to_s
+puts percentage.to_s.class
+puts percentage.to_i
+puts percentage.to_i.class
+
+puts
+
+puts "8".to_s
+puts "8".to_s.class
---
+++ the_class_method.rb
@@ -0,0 +1,22 @@
+# Class - Is a blueprint for an object (template/schematic)
+#Object - An "instance" of the class
+
+puts "hello".class
+puts "Ruby is fun".class
+puts "123".class
+puts "345$".class
+puts "".class
+
+puts
+
+puts 1.class
+puts 2002.class
+puts 001.class
+puts 55_666.class
+
+puts
+
+puts 2.67.class
+puts 0.00.class
+puts -3.09.class
+
