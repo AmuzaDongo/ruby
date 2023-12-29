13fe0b7adf891e745b324102c3871814893a55b9
AmuzaDongo <dongoamuza@gmail.com>
2023-12-29 09:26:56 +0300

    add get method

---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,4 @@
+puts "What is your name?"
+name = gets # read the string from the user
+
+puts "Your name is #{name}? Awesome!"
---
+++ the_gets_method.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+# name = gets # read the string from the user
+name = gets.chomp # chomp removes the new line character
+
+puts "Great, what's your age?"
+age = gets.chomp
+
+puts "Your name is #{name} and you are #{age} years old. Awesome!"
