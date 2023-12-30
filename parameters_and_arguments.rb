3330487db90f309d9795ac3cf21227553092a405
AmuzaDongo <dongoamuza@gmail.com>
2023-12-30 20:49:08 +0300

    add parameters and aguments

---
+++ parameters_and_arguments.rb
@@ -0,0 +1,10 @@
+def intro_person(name, age)
+  puts "I am #{name}"
+  puts "#{name} is #{age} years old"
+  puts "#{name} is handsome"
+  puts "#{name} is talented"
+  puts "#{name} is brilliant"
+end
+
+intro_person("Dongo Amuza", 27)
+intro_person("Mutoni", 26)
